3d033fbc78fae7096a4012108f360cb30e85c4a7
ShivaTejaV <116464067+ShivaTejaV@users.noreply.github.com>
2022-12-18 01:19:22 +0530

    Update index.js

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -1,16 +1,37 @@
 // Write your code here
+import {format} from 'date-fns'
 import './index.css'
 
 const AppointmentItem = props => {
-  const {date, title} = props
+  const {details, onStarred} = props
+  const {id, title, date, isStarred} = details
+
+  const formattedDate = format(new Date(`${date}`), 'dd MMMM yyyy, EEEE')
+
+  const starFunction = () => {
+    onStarred(id)
+  }
 
   let imgUrl
-
+  if (isStarred) {
+    imgUrl =
+      'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
+  } else {
+    imgUrl =
+      'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+  }
   return (
-    <div className="item">
-      <h1>{title}</h1>
-      <img />
-      <p>{date}</p>
-    </div>
+    <li className="item" key={id}>
+      <div className="titleContainer">
+        <h1 className="title">{title}</h1>
+        <button type="button" testid="star" onClick={starFunction}>
+          <img src={imgUrl} className="star" />
+        </button>
+      </div>
+
+      <p className="date">Date: {formattedDate}</p>
+    </li>
   )
 }
+
+export default AppointmentItem
